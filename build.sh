d01a8b7cb0733760e4afae154a0f21cf3d8cae00
ilgiova237 <giova2312yt@gmail.com>
2026-03-14 11:20:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o $HOME/yt-dlp
-chmod +x $HOME/yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./yt-dlp
+chmod +x ./yt-dlp
+echo "yt-dlp saved to: $(pwd)/yt-dlp"
+ls -la ./yt-dlp
